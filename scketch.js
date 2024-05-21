ef046b2d0a5abe13ff4e79afcb282a741c92d08a
lehalves17 <00001109167258sp@al.educacao.sp.gov.br>
2024-05-21 20:41:37 -0300

    js

--- scketch.js
+++ scketch.js
@@ -1,14 +1,14 @@
 function setup() {
-    createCanvas(400, 400);
-    background("pink")
-  }
+  createCanvas(400, 400);
+  background("pink")
+}
+
+function draw() {
+  stroke("blue");
+  fill("red");
   
-  function draw() {
-    stroke("blue");
-    fill("red");
-    
-    
-    if (mouseIsPressed) {
-      rect(mouseX, mouseY, 20, 35);
-    }
+  
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
   }
+}
